31bb18105f564fe1157293ada9f8bdd78b6df06e
Anthony Luque <kasuru@hotmail.com>
2021-09-12 23:41:03 -0500

    [ADD] validation when data device is null

--- src/components/CardShowDataDevice/index.js
+++ src/components/CardShowDataDevice/index.js
@@ -1,32 +1,41 @@
 /* eslint-disable react/jsx-pascal-case */
+import { useState } from "react";
 import { TextSmallNormal } from "../font/TexSmallNormal";
 import { TextMediumNormal } from "../font/TextMediumNormal";
 import { _Container } from "./styles";
 
 export const CardShowDataDevice = ({ data }) => {
+  const [display, setDisplay] = useState(null);
+  data === null && setTimeout(() => setDisplay("none"), 6000);
   return (
-    <_Container>
-      <TextMediumNormal>
-        {data["serial"] === null ? data["id"] : data["serial"]}
-      </TextMediumNormal>
-      <TextSmallNormal>Nombre: {data["name"]}</TextSmallNormal>
-      <TextSmallNormal>Tipo: {data["type"]}</TextSmallNormal>
-      <TextSmallNormal>SO: {data["operativeSystem"]}</TextSmallNormal>
-      {data["owner"] !== null ? (
+    <_Container style={{ display: display }}>
+      {data === null ? (
+        <TextMediumNormal>No se encotro el dispositivo</TextMediumNormal>
+      ) : (
         <div>
+          <TextMediumNormal>
+            {data["serial"] === null ? data["id"] : data["serial"]}
+          </TextMediumNormal>
+          <TextSmallNormal>Nombre: {data["name"]}</TextSmallNormal>
+          <TextSmallNormal>Tipo: {data["type"]}</TextSmallNormal>
+          <TextSmallNormal>SO: {data["operativeSystem"]}</TextSmallNormal>
+          {data["owner"] !== null ? (
+            <div>
+              <TextSmallNormal>
+                Propietario: {data["owner"].name} {data["owner"].lastname}
+              </TextSmallNormal>
+              <TextSmallNormal>
+                Email propietario: {data["owner"].email}
+              </TextSmallNormal>
+            </div>
+          ) : null}
+
           <TextSmallNormal>
-            Propietario: {data["owner"].name} {data["owner"].lastname}
-          </TextSmallNormal>
-          <TextSmallNormal>
-            Email propietario: {data["owner"].email}
+            Fecha de asignación:{" "}
+            {new Date(Number(data["assignedAt"])).toLocaleDateString("es-ES")}
           </TextSmallNormal>
         </div>
-      ) : null}
-
-      <TextSmallNormal>
-        Fecha de asignación:{" "}
-        {new Date(Number(data["assignedAt"])).toLocaleDateString("es-ES")}
-      </TextSmallNormal>
+      )}
     </_Container>
   );
 };
